75c172260ce0c4fb88c77555e9da1c562ac892dd
Mike Williamson <mike.williamson@cds-snc.ca>
2018-10-25 17:52:26 -0400

    Add getBaseline function

---
+++ src/__tests__/getBaseline.test.js
@@ -0,0 +1,54 @@
+global.fetch = require('jest-fetch-mock')
+const { getBaseline } = require('../getBaseline')
+
+describe('getBaseline', () => {
+  it('raises an error when called with no arguments', async () => {
+    try {
+      await getBaseline()
+    } catch ({ message }) {
+      expect(message).toMatch(/you must provide a url/)
+    }
+  })
+
+  it('raises an error when called with a bad URL', async () => {
+    try {
+      await getBaseline('not a url')
+    } catch ({ message }) {
+      expect(message).toMatch(/not a valid URL/)
+    }
+  })
+
+  it(`raises an error when the URL isn't pointing to a .yaml file`, async () => {
+    try {
+			await getBaseline('https://example.com/fluffy-kittens.jpg')
+    } catch ({ message }) {
+      expect(message).toMatch(/needs to point to a YAML file/)
+    }
+  })
+
+  it('returns a baseline object given a URL', async () => {
+    fetch.mockResponseOnce(`
+name: light
+standards:
+  ITSG-33a:
+    AC-2: {}
+    AC-3: {}
+    AC-6: {}
+    AU-2: {}
+    AU-6: {}
+`)
+    let baseline = await getBaseline('https://example.com/baseline.yaml')
+    expect(baseline).toEqual({
+      name: 'light',
+      standards: {
+        'ITSG-33a': {
+          'AC-2': {},
+          'AC-3': {},
+          'AC-6': {},
+          'AU-2': {},
+          'AU-6': {},
+        },
+      },
+    })
+  })
+})
