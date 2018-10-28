f7c8e75d31d9ee369dcff7d4c3b931027cea2606
Mike Williamson <mike@korora.ca>
2018-10-28 12:01:41 -0400

    Add env vars for compliance checking (#9)
    This commit adds env vars for the certification, the definitions and the
    path to the checks folder. The existence of these are confirmed at
    startup by dotenv-safe based on the example values provided by
    .env.example.
    
    The createCompliance function is then used to create a root object for
    the graphql api to expose.

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
+require("dotenv-safe").config();
+require("isomorphic-fetch");
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const { schema } = require("./src/schema");
+const { getChecks } = require("./src/getChecks.js");
+const { fetchYaml } = require("./src/fetchYaml.js");
+const { createCompliance } = require("./src/createCompliance.js");
+(async () => {
+  const {
+    CHECKS_PATH: checksPath,
+    DEFINITIONS_URL: definitionsUrl,
+    CERTIFICATION_URL: certificationUrl
+  } = process.env;
 
-let server = express();
+  let server = express();
 
-server.use("/", graphqlHTTP({ schema, graphiql: true }));
+  let checks = await getChecks(checksPath);
+  let certification = await fetchYaml(certificationUrl);
+  let definitions = await fetchYaml(definitionsUrl);
 
-server.listen(3000);
+  let compliancePosture = await createCompliance({
+    definitions,
+    checks,
+    certification
+  });
+
+  server.use(
+    "/",
+    graphqlHTTP({ schema, graphiql: true, rootValue: compliancePosture })
+  );
+
+  server.listen(3000);
+})();
