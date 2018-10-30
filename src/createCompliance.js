1e31163f991c54ef99461f3240b735e0d101fd7c
Mike Williamson <mike@korora.ca>
2018-10-30 15:00:34 -0400

    Ignore controls that aren't in the certification (#15)

--- src/createCompliance.js
+++ src/createCompliance.js
@@ -1,35 +1,39 @@
-const isEmpty = (suspect = {}) => Object.keys(suspect).length === 0;
+const isEmpty = (suspect = {}) => Object.keys(suspect).length === 0
 
 const makeSchemaFriendly = str =>
-  str.replace(/(\w{2})-(\d{1,2})/, "$1_$2").replace(/ \((\d{1,2})\)/, "_$1");
+  str.replace(/(\w{2})-(\d{1,2})/, '$1_$2').replace(/ \((\d{1,2})\)/, '_$1')
 
 module.exports.createCompliance = async ({
   definitions,
   checks,
-  certification
+  certification,
 }) => {
   let {
-    standards: { "ITSG-33a": itsg }
-  } = certification;
+    standards: { 'ITSG-33a': itsg },
+  } = certification
 
   let compliance = Object.keys(itsg).reduce((cplnc, key) => {
-    cplnc[makeSchemaFriendly(key)] = definitions[key];
-    return cplnc;
-  }, {});
+    cplnc[makeSchemaFriendly(key)] = definitions[key]
+    return cplnc
+  }, {})
+
+  let controlsWeCareAbout = Object.keys(itsg)
 
   checks.forEach(check => {
     check.satisfies.forEach(ctl => {
-      if (!compliance[makeSchemaFriendly(ctl)].verifications) {
-        compliance[makeSchemaFriendly(ctl)].verifications = [];
+      if (controlsWeCareAbout.includes(ctl)) {
+        if (!compliance[makeSchemaFriendly(ctl)].verifications) {
+          compliance[makeSchemaFriendly(ctl)].verifications = []
+        }
+
+        compliance[makeSchemaFriendly(ctl)].verifications = [
+          ...compliance[makeSchemaFriendly(ctl)].verifications,
+          check,
+        ]
       }
+    })
+  })
 
-      compliance[makeSchemaFriendly(ctl)].verifications = [
-        ...compliance[makeSchemaFriendly(ctl)].verifications,
-        check
-      ];
-    });
-  });
-
-  certification.standards["ITSG-33a"] = compliance;
-  return certification;
-};
+  certification.standards['ITSG-33a'] = compliance
+  return certification
+}
